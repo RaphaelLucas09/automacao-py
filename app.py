8831ee653f591a11a25d707b10791f5781059689
RaphaelLucas09 <niel9591@gmail.com>
2023-09-22 22:54:18 -0300

    mudança alibaba

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://www.instagram.com/accounts/login/")
     page.get_by_label("Telefone, nome de usuário ou email").click()
-    page.get_by_label("Telefone, nome de usuário ou email").fill("fael9591@gmail.com")
+    page.get_by_label("Telefone, nome de usuário ou email").fill(
+        "fael9591@gmail.com")
     page.get_by_label("Telefone, nome de usuário ou email").press("Tab")
     page.get_by_label("Senha").fill("@uama1252")
     page.get_by_role("button", name="Entrar", exact=True).click()
-    page.get_by_label("Nova publicação").click()
-    page.get_by_role("button", name="Selecionar do computador", exact=True).click()
+    page.get_by_role("button", name="Agora não").click()
+    page.get_by_role("button", name="Agora não").click()
+    page.get_by_role("link", name="Nova publicação Criar").click()
+    page.get_by_role("button", name="Selecionar do computador",
+                     exact=True).click()
+    page.get_by_role("button", name="alibaba").click()
     # ---------------------
     context.storage_state(path="state.json")
-    context.close()
-    browser.close()
+    
+    x = 0
+    for x in range(10):
+        return x
+    if x == 10:
+        context.close()
+        browser.close()
 
 
 with sync_playwright() as playwright:
